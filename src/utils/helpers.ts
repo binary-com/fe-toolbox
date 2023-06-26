b8b367f1409c02bddaa1f6514fb1ae3d69de3e24
Jim Daniels Wasswa <jim@deriv.com>
2023-06-26 15:08:48 +0800

    chore: add helper function to extract version from task name

---
+++ src/utils/helpers.ts
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {String}  task_name // The name of the task from which the version will be extracted
+ * @returns {String} // The version string extracted from the task name
+ */
+const extractVersionFromTaskName = (task_name = '') => {
+    const regex = /V\d+/; // Match the version string (V followed by digits)
+
+    const matches = task_name.match(regex);
+
+    return matches && matches.length > 0 ? matches[0] : ''; // Extract the first matched string if it exists
+};
