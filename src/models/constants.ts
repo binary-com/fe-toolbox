b5c6c73b4128e1d83c0a540944afa073d9941c6f
Jim Daniels Wasswa <jim@deriv.com>
2023-06-28 13:29:10 +0800

    chore: resolve whitespace issue

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -3,7 +3,6 @@ export const PULL_REQUEST_CHECKS_TIMEOUT = 1 * 60 * 1000; // 1 minute
 export const PULL_REQUEST_REFETCH_TIMEOUT = 5 * 1000; // 5 seconds
 export const PULL_REQUEST_REFETCH_LIMIT = 10; // the max amount of refetches to check for a pull request's status checks
 export const PULL_REQUEST_CHECKS_LIMIT = 120;
-
 export const CIRCLECI_API_URL = 'https://circleci.com/api/v2';
 export const CLICKUP_API_URL = 'https://api.clickup.com/api/v2';
 export const CLICKUP_STATUSES = {
